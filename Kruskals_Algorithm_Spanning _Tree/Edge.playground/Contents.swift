114c9de7f16b5a95bc740214bc60feef1e0ecef3
Maria shehata <maria.sheahata17@ncf.edu>
2020-02-20 21:34:04 -0500

    added Edge

---
+++ Kruskals_Algorithm_Spanning _Tree/Edge.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+/*
+ This class defines the Edge struct. The Edge is the main unit of the graph. Each edge has a weight that will be used to compare the graph edges to build the minimum spanning tree
+*/
+
+struct Edge{
+    
+    var vertex1 : Int
+    var vertex2 : Int
+    var weight :Int
+    
+    init(vertex1 : Int, vertex2 : Int, weight :Int ){
+        self.vertex1 = vertex1
+        self.vertex2 = vertex2
+        self.weight = weight
+    }
+    
+    func either() -> Int {
+        return vertex1
+    }
+    
+    func other( vertex : Int )-> Int {
+        if vertex == vertex1 {return vertex2}
+        return vertex1
+    }
+    
+}
+
+extension Edge :Comparable {
+    
+    static func <( edge1 : Edge , edge2 : Edge) -> Bool{
+        return edge1.weight < edge2.weight
+    }
+    
+}
